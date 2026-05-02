526f16240363e4cb4d7cc6f9d530eccbe8a56baf
Chaithra-1408 <mc1104@srmist.edu.in>
2026-05-03 01:30:18 +0530

    UC10: Count total seats using Stream reduce()

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -79,5 +79,27 @@ public class TrainConsistManagementApp {
         System.out.println("\nVerification: Original list size remains "
                 + allBogies.size());
         System.out.println("====================================");
+
+        System.out.println("\n--- UC10: Count Total Seats in Train ---");
+
+        List<Bogie> bogies = new ArrayList<>();
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 56));
+        bogies.add(new Bogie("First Class", 24));
+        bogies.add(new Bogie("Sleeper", 70));
+
+        System.out.println("Bogies in Train:");
+        for (Bogie b : bogies) {
+            System.out.println(b.name + " -> " + b.capacity);
+        }
+
+        int totalSeats = bogies.stream()
+                .map(b -> b.capacity)
+                .reduce(0, Integer::sum);
+
+        System.out.println("\nTotal Seating Capacity of Train: "
+                + totalSeats);
+        System.out.println("\nUC10 aggregation completed...");
+        System.out.println("====================================");
     }
 }
